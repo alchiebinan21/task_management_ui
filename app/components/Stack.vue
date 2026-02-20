314b2a3998f8acfd5c1e24c836fa28a1b940d496
Altsi <alchiebinan21@gmail.com>
2026-02-20 14:32:47 +0100

    updated stack component

--- app/components/Stack.vue
+++ app/components/Stack.vue
@@ -2,31 +2,51 @@
     <div class="flex flex-col gap-[50px]">
         <div class="flex flex-row justify-center items-center gap-[50px]">
                 <div class="stack-item">
-                    <img src="/logos/logo-black.png" alt="Nuxt Js3  Logo" class="h-[50px] inline-block mr-2 ease-in-out" 
+                    <a href="https://nuxt.com/" target="_blank">
+                        <img src="/logos/logo-black.png" alt="Nuxt Js3  Logo" class="h-[50px] inline-block mr-2 ease-in-out" 
                         :class="{ 'invert': isDark }" 
-                    />
+                        />
+                    </a>
                 </div>
                 <div class="stack-item">
-                    <img src="/logos/Amazon-Web-Services-Emblem.png" alt="AWS Logo" class="h-[100px] inline-block mr-2" />
+                    <a href="https://aws.amazon.com/" target="_blank">
+                        <img src="/logos/Amazon-Web-Services-Emblem.png" alt="AWS Logo" class="h-[100px] inline-block mr-2" />
+                    </a>
                 </div>
                 <div class="stack-item">
-                    <img src="/logos/laravel-logo.png" alt="AWS Logo" class="h-[70px] inline-block mr-2" />
+                    <a href="https://laravel.com/" target="_blank">
+                        <img src="/logos/laravel-logo.png" alt="AWS Logo" class="h-[70px] inline-block mr-2" />
+                    </a>
                 </div>
         </div>
         <div class="flex flex-row justify-center items-center gap-[50px]">
             <div class="stack-item flex flex-col items-center text-black transition duration-300 ease-in-out" :class="{ 'invert': isDark }"
                 >
-                <img src="/logos/mcp.svg" alt="MCP Logo" class="h-[100px] inline-block mr-2 brightness-0" 
-                />
+                <a href="https://modelcontextprotocol.io/" target="_blank">
+                    <img src="/logos/mcp.svg" alt="MCP Logo" class="h-[100px] inline-block mr-2 brightness-0" 
+                        :class="{ 'invert brightness-0': isDark }" 
+                    />
+                </a>
                 <span class="block w-full text-md text-black text-center">Model Context Protocol</span>
             </div>
             <div class="stack-item">
-                <img src="/logos/python.svg" alt="Python Logo" class="h-[200px] inline-block mr-2 transition duration-300 ease-in-out" 
-                    :class="{ 'invert brightness-0': isDark }" 
-                />
+                <a href="https://www.python.org/" target="_blank">
+                    <img src="/logos/python.svg" alt="Python Logo" class="h-[200px] inline-block mr-2 transition duration-300 ease-in-out" 
+                        :class="{ 'invert brightness-0': isDark }" 
+                    />
+                </a>
             </div>
             <div class="stack-item">
-                <img src="/logos/redis-logo.svg" alt="Redis Logo" class="h-[100px] inline-block mr-2" />
+                <a href="https://redis.io/" target="_blank">
+                    <img src="/logos/redis-logo.svg" alt="Redis Logo" class="h-[100px] inline-block mr-2" />
+                </a>
+            </div>
+        </div>
+        <div class="flex flex-row justify-center items-center gap-[50px]">
+            <div class="stack-item">
+                <a href="https://www.mysql.com/" target="_blank">
+                    <img src="/logos/logo-mysql.svg" alt="MySQL Logo" class="h-[100px] inline-block mr-2" />
+                </a>
             </div>
         </div>
     </div>
